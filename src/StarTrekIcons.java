2d55c4a09bfdf9e9821594c4ee46cd9e7ce880de
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2019-09-07 18:24:12 +0200

    updated images

--- src/StarTrekIcons.java
+++ src/StarTrekIcons.java
@@ -3,6 +3,6 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface StarTrekIcons {
-    Icon CAT_ICON = IconLoader.getIcon("/rsz_cat.png");
-    Icon RCAT_ICON = IconLoader.getIcon("/rsz_rcat.png");
+    Icon CAT_ICON = IconLoader.getIcon("/rsz_uss_enterprise.png");
+    Icon RCAT_ICON = IconLoader.getIcon("/rsz_russ_enterprise.png");
 }
