0ade488297aaf4e7d0cc15fccf58fe9449b6c35d
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2019-09-07 18:10:05 +0200

    updated images

--- src/JuventusIcons.java
+++ src/JuventusIcons.java
@@ -3,6 +3,6 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface JuventusIcons {
-    Icon CAT_ICON = IconLoader.getIcon("/rsz_cat.png");
-    Icon RCAT_ICON = IconLoader.getIcon("/rsz_rcat.png");
+    Icon CAT_ICON = IconLoader.getIcon("/rsz_juventus.png");
+    Icon RCAT_ICON = IconLoader.getIcon("/rsz_rjuventus.png");
 }
