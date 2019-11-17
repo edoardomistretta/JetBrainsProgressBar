1043ddd90e5a50e7b00a37e87eb9e9b1d7eb5bde
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2019-11-17 15:32:30 +0100

    updated images name

--- src/FacileItIcons.java
+++ src/FacileItIcons.java
@@ -3,6 +3,6 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface FacileItIcons {
-    Icon CAT_ICON = IconLoader.getIcon("/rsz_cat.png");
-    Icon RCAT_ICON = IconLoader.getIcon("/rsz_rcat.png");
+    Icon CAT_ICON = IconLoader.getIcon("/rsz_facileit.png");
+    Icon RCAT_ICON = IconLoader.getIcon("/rsz_rfacileit.png");
 }
